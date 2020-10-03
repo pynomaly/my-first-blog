9ea5214262af93ad2c6115004d861d8bb861099b
anomalia <anavgo@gmail.com>
2020-10-03 19:53:17 +0200

    Agregadas vistas y plantillas

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 #Nuestro primer patrón URL
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
